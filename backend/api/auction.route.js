61c1bb58c563c9db3d98a7cd2dda4b98d2bc35ae
Davin <davinmailsklh@gmail.com>
2022-09-04 11:01:29 +0700

    Added jwt Authentication

--- backend/api/auction.route.js
+++ backend/api/auction.route.js
@@ -5,8 +5,10 @@ const bcrypt = require('bcrypt')
 const router = express.Router()
 
 const user = require('./schemas/user.js')
+const item = require('./schemas/item.js')
 const validateUser = require('./validationSchemas/validateUser.js')
-const { json } = require('express')
+
+
 
 const validateReq = (schema) => async (req, res, next) => {
     const reqBody = req.body
@@ -47,6 +49,59 @@ router.post('/login', async (req, res) => {
         }
     })
 })
+
+router.post('/items', authenticateToken, async (req, res) => {
+    const newItem = new item({
+        name: req.body.name,
+        image: req.body.image || null,
+        price: req.body.price,
+        userId: req.user._id
+    })
+
+    try{
+        const addItem = await newItem.save()
+        res.status(201).json({message: "Item added"})        
+    }catch(e){
+        res.status(400).json(e.message)
+    }
+})
+
+router.get('/items', authenticateToken, async (req, res) => {
+    const itemsPerPage = 10
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0
+    let cursor
+
+    try{
+        cursor = await item.find().limit(itemsPerPage).skip(itemsPerPage * page)
+    }catch(e){
+        res.status(500).json(e.message)
+    }
+
+    try{
+        const itemList = cursor
+        res.status(200).json({items: itemList})
+    }catch(e){
+        res.status(500).json(e.message)
+    }
+    
+    
+})
+
+
+function authenticateToken(req, res, next){
+    const authHeader= req.headers["authorization"]
+    const token = authHeader
+    console.log(token)
+    if (token == null) return res.status(401).json({message: "Token not provided"})
+
+    jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (e, user) => {
+        if(e) return res.status(401).json({message: e.message})
+        console.log(user)
+        req.user = user
+        next()
+    })
+}
+
 
 
 
